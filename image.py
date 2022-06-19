235048965dbad71d077443f5757609085daec187
Volodymyr Parakhonych <99834423+parakhonych@users.noreply.github.com>
2022-06-20 01:47:45 +0200

    Added classes Sub_Image, Sub_Window. Added functions open (for grayscale and color)

---
+++ image.py
@@ -0,0 +1,27 @@
+import cv2
+from PyQt5.QtWidgets import QMdiSubWindow, QLabel
+from PyQt5.QtGui import QImage, QPixmap
+class Sub_Image:
+    def __init__(self, image_name, image_data, image_number, image_gray):
+        self.name = image_name
+        self.data = image_data
+        self.number = image_number
+        self.gray = image_gray
+        self.sub = Sub_Window(self.name, self.data, self.number)
+        #print(image_gray)
+
+class Sub_Window(QMdiSubWindow):
+    def __init__(self, img_name, img_data, img_number):
+        super().__init__()
+        self.title = img_name
+        self.sub_data = img_data
+        self.number = img_number
+        self.image_label = QLabel()
+        self.setWidget(self.image_label)
+        self.setWindowTitle(self.title)
+        rgb_image = cv2.cvtColor(self.sub_data , cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        tempimage = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        self.pixmap = QPixmap(tempimage)
+        self.image_label.setPixmap(self.pixmap.copy())
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import sys
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from image import Sub_Image
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMdiArea, QAction
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMdiArea, QAction, QFileDialog
 
 class UI(QMainWindow):
-    couter = 0
+    counter = 0
     def __init__(self):
         super(UI, self).__init__()
         self.windows = dict()
@@ -23,7 +24,56 @@ class UI(QMainWindow):
         self.action_open_grayscale = self.findChild(QAction, "actionGrayscale")
         self.action_open_color = self.findChild(QAction, "actionColor")
 
+        #Click Button
+        self.action_open_grayscale.triggered.connect(self.open_windows_gray)
+        self.action_open_color.triggered.connect(self.open_windows_color)
+
         self.show()
+
+    def open_windows_gray(self):
+        f_paths, _ = QFileDialog.getOpenFileNames(self, "Open file", "D:\\Test_Images\\", "All Files (*);;"
+                                                                                          "BMP files(*.bmp);;"
+                                                                                         "JPEG files (*.jpeg *.jpg);;"
+                                                                                         "PNG(*.png);;"
+                                                                                         "TIFF files (*.tiff *.tif)")
+        if not f_paths:
+            return
+        for f_path in f_paths:
+
+            self.open_window_gray(f_path)
+
+    def open_window_gray(self, fname):
+        if fname:
+            name = fname.split("/")[-1]
+            img = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+            UI.counter = UI.counter + 1
+            image = Sub_Image(name, img, UI.counter, True)
+            self.windows[UI.counter] = image
+            self.mdi.addSubWindow(image.sub)
+            image.sub.show()
+    def open_windows_color(self):
+        f_paths, _ = QFileDialog.getOpenFileNames(self, "Open file", "D:\\Test_Images\\", "All Files (*);;"
+                                                                                          "BMP files(*.bmp);;"
+                                                                                         "JPEG files (*.jpeg *.jpg);;"
+                                                                                         "PNG(*.png);;"
+                                                                                         "TIFF files (*.tiff *.tif)")
+        if not f_paths:
+            return
+        for f_path in f_paths:
+
+            self.open_window_color(f_path)
+
+    def open_window_color(self, fname):
+        if fname:
+            name = fname.split("/")[-1]
+            img = cv2.imread(fname, cv2.IMREAD_UNCHANGED)
+            UI.counter = UI.counter + 1
+            image = Sub_Image(name, img, UI.counter, False)
+            self.windows[UI.counter] = image
+            self.mdi.addSubWindow(image.sub)
+            image.sub.show()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     UIWindow = UI()
